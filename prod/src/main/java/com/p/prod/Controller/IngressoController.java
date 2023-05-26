a66a326f46122c2c5b30e982914fdecac59462f0
WesllenWorld <wesllen.2020111052@unicap.br>
2023-05-25 23:30:18 -0300

    fix no controller

--- prod/src/main/java/com/p/prod/Controller/IngressoController.java
+++ prod/src/main/java/com/p/prod/Controller/IngressoController.java
@@ -4,19 +4,33 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.p.prod.Entity.*;;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import static lombok.AccessLevel.PRIVATE;
+import com.p.prod.Entity.*;
+import com.p.prod.Producer.Producer;
+import java.util.UUID;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
+@RequestMapping("/ingressos")
+@RequiredArgsConstructor
+@FieldDefaults(level = PRIVATE, makeFinal = true)
 public class IngressoController {
     
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
+    Producer<Ingresso> ingressoProducer;
 
-    @PostMapping("/sell")
-    public String sellTicket(@RequestBody Ingresso ticket) {
-        rabbitTemplate.convertAndSend("ticket-exchange", "sell-ticket", ticket);
-        return "Ticket sent to the queue for processing.";
+    @PostMapping(path = "", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    @ResponseStatus(CREATED)
+    public Ingresso comprarIngresso(@RequestBody Ingresso ingresso) {
+
+        ingresso.setId(UUID.randomUUID().toString());
+        ingressoProducer.produce(ingresso);
+
+        return ingresso;
     }
 }
